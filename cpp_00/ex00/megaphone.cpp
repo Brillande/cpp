f51f319bec20f92e12928371a4423ca75f014ea2
brillande <emedina-@DESKTOP-GHPVO1T>
2024-09-05 12:56:48 +0200

    ex00 finished

---
+++ cpp_00/ex00/megaphone.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <cstdlib>
+using namespace std;
+
+int main(int ac, char **av)
+{
+    int i;
+    int j;
+
+    i = 1;
+    if(ac < 2)
+    {
+        cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *\n";
+        exit(EXIT_FAILURE);
+    }
+    while(i < ac)
+    {
+        j = 0;
+        while(av[i][j] != '\0')
+        {
+            cout << (char)toupper(av[i][j]);
+            j++;
+        }
+        i++;
+    }
+    cout << "\n";
+}
