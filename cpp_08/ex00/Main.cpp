c8723d17b79461e80f408d3f2d65ecee1c836d3c
brillande <jelland90@gmail.com>
2025-05-13 20:55:53 +0200

    ex00 ended

--- cpp_08/ex00/Main.cpp
+++ cpp_08/ex00/Main.cpp
@@ -4,26 +4,26 @@ int main()
 {
     std::vector<int> numbers;
     for (int i = 0; i < 10; ++i)
-        numbers.push_back(i * 2); // 0, 2, 4, ..., 18
+        numbers.push_back(i); // 0, 2, 4, ..., 18
 
     try 
     {
         int *found = easyfind(numbers, 8);
-        std::cout << "Encontrado: " << *found << std::endl;
+        std::cout << "finded: " << *found << std::endl;
     }
     catch (const std::exception &e)
     {
-        std::cout << "No encontrado: 8" << std::endl;
+        std::cout << "dont finded: 8" << std::endl;
     }
 
     try 
     {
-        int *found = easyfind(numbers, 7);
-        std::cout << "Encontrado: " << *found << std::endl;
+        int *found = easyfind(numbers, 10);
+        std::cout << "finded: " << *found << std::endl;
     }
     catch (const std::exception &e) 
     {
-        std::cout << "No encontrado: 7" << std::endl;
+        std::cout << "dont finded: 10" << std::endl;
     }
 
     return 0;
